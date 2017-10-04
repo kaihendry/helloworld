8760966a7fae4f994ebfada0bad4b66b597c9b0a
Kai Hendry <hendry@webconverger.com>
2017-10-04 16:37:50 +0800

    Example

--- main.go
+++ main.go
@@ -1,40 +1,25 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
-	"strconv"
 	"time"
 )
 
 func main() {
 
-	http.HandleFunc("/", slow)
+	http.HandleFunc("/fast", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintln(w, "fast") })
+
+	http.HandleFunc("/slow", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration(2) * time.Second)
+		fmt.Fprintln(w, "slow")
+	})
 
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatalf("error listening: %s", err)
 	}
-
-}
-
-func slow(w http.ResponseWriter, r *http.Request) {
-	i, err := strconv.Atoi(r.URL.Path[1:])
-	if err != nil {
-		i = 0
-	}
-
-	after := time.Duration(i)
-
-	log.Println("Sleep for", i)
-
-	time.Sleep(after * time.Second)
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(struct {
-		Delay int `json:"delay"`
-	}{Delay: i})
 
 }
