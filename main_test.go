5bcbccc831aad68851780c332757419c1a6081e6
Kai Hendry <hendry@webconverger.com>
2017-10-05 11:41:18 +0800

    Benchmarking

--- main_test.go
+++ main_test.go
@@ -1,38 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestParameters(t *testing.T) {
-	testCases := map[string]struct {
+	testCases := []struct {
 		params     map[string]string
 		statusCode int
 	}{
-		"fast": {
+		{
 			map[string]string{"delay": "0"},
 			http.StatusOK,
 		},
-		"slow": {
+		{
 			map[string]string{"delay": "2"},
+			http.StatusOK,
+		},
+		{
+			map[string]string{"delay": "1"},
 			http.StatusOK,
 		},
 	}
 
-	for tc, tp := range testCases {
-		req, _ := http.NewRequest("GET", "/", nil)
-		q := req.URL.Query()
-		for k, v := range tp.params {
-			q.Add(k, v)
-		}
-		req.URL.RawQuery = q.Encode()
-		rec := httptest.NewRecorder()
-		root(rec, req)
-		res := rec.Result()
-		if res.StatusCode != tp.statusCode {
-			t.Errorf("`%v` failed, got %v, expected %v", tc, res.StatusCode, tp.statusCode)
-		}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%s", tc.params), func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "/", nil)
+			q := req.URL.Query()
+			for k, v := range tc.params {
+				q.Add(k, v)
+			}
+			req.URL.RawQuery = q.Encode()
+			rec := httptest.NewRecorder()
+			root(rec, req)
+			res := rec.Result()
+			if res.StatusCode != tc.statusCode {
+				t.Errorf("`%v` failed, got %v, expected %v", tc, res.StatusCode, tc.statusCode)
+			}
+		})
 	}
 }
